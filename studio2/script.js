6aca97ff86ad37411e7d9d2643daf674b1afe736
Reiley <reileyjohnson@gmail.com>
2023-11-08 17:15:24 -0800

    optimized studio 2 js

--- studio2/script.js
+++ studio2/script.js
@@ -1,46 +1,45 @@
 (function(){
     'use strict';
-    console.log(`running JS!`);
-    
-    const hotSpots = document.querySelectorAll('figure div') //makes a variable for all hotspots
-    const hoverPic = document.querySelectorAll('.hoverPic') //makes a variable for all overlays that should appear when hovering
+    console.log('running js');
+
+    let figArray = []; //empty array for each ID of a figure to be pushed into later
+    let hotSpotArray = []; //empty array for each div in a figure to be pushed into later
+
+    const figures = document.querySelectorAll('figure'); // makes array of all figures
+    const hotSpots = document.querySelectorAll('figure div'); // makes array of divs inside figures
+    const hoverPic = document.querySelectorAll('.hoverPic'); // makes array of all imgs with class of hoverPic
+
+    for(const eachFig of figures){
+        figArray.push(eachFig.id); //puts IDs of each figure into figArray
+    };
+    //console.log(figArray);
+
+    for (const eachHotSpot of hotSpots){
+        hotSpotArray.push(eachHotSpot.id);  //puts every id of a div in a figure into hotSpotArray
+    }
+    //console.log(hotSpotArray);
 
     hotSpots.forEach(function(eachSpot){
-        eachSpot.addEventListener('mouseover', showHoverPic); //tells browser what to do when mouse is hovering over a div
-        eachSpot.addEventListener('mouseout', function(){ //tells browser what to do when mouse stops hovering over that div
-            hoverPic.forEach(function(eachHoverPic){
-                eachHoverPic.className = 'hoverPic hidden';
-            }); // for each function so browser knows when mouseout of that div every hoverPic becomes hidden.
-        })
-    }); // for each function so browser knows when the mouse is hovering over or not hovering over each hotspot
+        eachSpot.addEventListener('mouseover', showHoverPic); //tells browser to call showHoverPic function when mouse hovers over a hot spot
+        eachSpot.addEventListener('mouseout', hideHoverPic); //tells browser to call hideHoverPic function when mouse stops hovering over a hot spot
+    });
 
     function showHoverPic(e){
-        const specificDiv = e.target.id; //makes a const for each div ID.
-        //console.log(specificDiv); //console tells me which div I'm hovering over
+        const divId = e.target.id;
+        //console.log(divId);
 
-        switch (specificDiv){
-            case 'ts1': hoverPic[0].className = 'hoverPic showing'; break;
-            case 'ts2': hoverPic[1].className = 'hoverPic showing'; break;
-            case 'ts3': hoverPic[2].className = 'hoverPic showing'; break;
-            case 'm1': hoverPic[3].className = 'hoverPic showing'; break;
-            case 'm2': hoverPic[4].className = 'hoverPic showing'; break;
-            case 'm3': hoverPic[5].className = 'hoverPic showing'; break;
-            case 'z1': hoverPic[6].className = 'hoverPic showing'; break;
-            case 'z2': hoverPic[7].className = 'hoverPic showing'; break;
-            case 'z3': hoverPic[8].className = 'hoverPic showing'; break;
-            case 'f1': hoverPic[9].className = 'hoverPic showing'; break;
-            case 'f2': hoverPic[10].className = 'hoverPic showing'; break;
-            case 'f3': hoverPic[11].className = 'hoverPic showing'; break;
-        }; // when each div is hovered over, it shows its corresponding picture, organized as a member of the hoverPic array and specified by its order in the array
+        switch(divId){
+            case `${divId}`: document.querySelector(`.${divId}`).className = `hoverPic showing ${divId}`; break; //utilizes that overlay images have a class named after the ID of the div they are in: hovering over the div with a certain div id will give the overlay image with the corresponding class name a class name of 'showing'
+        }
+    };
+
+    function hideHoverPic(e){
+        const divId2 = e.target.id;
+        console.log(divId2);
+
+        switch(divId2){
+            case `${divId2}`: document.querySelector(`.${divId2}`).className = `hoverPic hidden ${divId2}`; break;
+        }
     }
 
-/*     hotSpots.forEach( function(eachSpot){
-        eachSpot.addEventListener('mouseover', function(){
-            console.log(`ur mousing overrrrrr`);
-        });
-        eachSpot.addEventListener('mouseout', function(){
-            console.log(`you're mousing outtttt`);
-        });
-    });
- */ // checked that mouseover & mouseout on each div was done correctly by telling me in the console log.
 })();
