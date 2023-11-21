b21d22088cb59b13bac79f352ad261849d72c555
Reiley <reileyjohnson@gmail.com>
2023-11-21 11:09:17 -0800

    attempt to fix GothicPixels font

--- studio3/script.js
+++ studio3/script.js
@@ -1,4 +1,34 @@
 (function(){
     'use strict'
-    console.log('running JS');
+    //console.log('running JS');
+
+    //Characters
+    const zomb = document.querySelector('#zomb');
+    const ghoul = document.querySelector('#ghoul');
+    const necro = document.querySelector('#necro');
+    const vamp = document.querySelector('#vamp');
+
+    //Players 
+    const player1 = document.querySelector('#p1');
+    const player2 = document.querySelector('#p2');
+
+    //Directions
+    const playerDirections = document.querySelector('#directions');
+    const turnDirections = document.querySelector('#turnDirections');
+
+    //HP
+    const p1HP = document.querySelector('#p1HP');
+    const p2HP = document.querySelector('#p2HP');
+
+    let attacker;
+    let defender;
+    let defenderIndex;
+    const gameData = {
+        monsters: [zomb, ghoul, necro, vamp],
+        health: [100, 100],
+        strongAttack: [0, 20, 40],
+        medAttack: [0, 15, 30],
+        weakAttack: [5, 10, 15],
+    };
+
 })();
